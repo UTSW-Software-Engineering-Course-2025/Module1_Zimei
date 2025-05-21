cd258130949d755cf2d78b8d635d8dbb8c46a803
ZM-C-777 <zmc_life@outlook.com>
2025-05-21 11:31:37 -0500

    update doc for adjustbeta

--- day1/tsne_practice/adjustbeta.py
+++ day1/tsne_practice/adjustbeta.py
@@ -1,8 +1,8 @@
 import numpy as np
-
+import torch
 
 def Hbeta(D, beta=1.0):
-    """
+    r"""
     Compute entropy(H) and probability(P) from nxn distance matrix.
 
     Parameters
@@ -19,6 +19,18 @@ def Hbeta(D, beta=1.0):
         entropy
     P : numpy.ndarray
         probability matrix (n,n)
+    
+    Examples
+    --------
+    >>> D = np.array([[0, 1], [1, 0], [2, 2]])
+    >>> beta = 1.0
+    >>> H, P = Hbeta(D, beta)
+    >>> print(H)
+    [1.2571852 1.2571852]
+    >>> print(P)
+    [[0.66524096 0.24472847]
+    [0.24472847 0.66524096]
+    [0.09003057 0.09003057]]
     """
     num = np.exp(-D * beta)
     den = np.sum(np.exp(-D * beta), 0)
@@ -39,6 +51,10 @@ def adjustbeta(X, tol, perplexity):
         tolerance for the stopping criteria of beta adjustment
     perplexity : float
         perplexity can be interpreted as a smooth measure of the effective number of neighbors
+    
+    Raises
+    ----------
+        ValueError: If the input tensor is not 2D.
 
     Returns
     -------
@@ -46,9 +62,29 @@ def adjustbeta(X, tol, perplexity):
         probability matrix (n,n)
     beta : numpy.ndarray
         precision array (n,1)
+    
+    Examples
+    --------
+    >>> X = np.array([[0, 1], [1, 0], [2, 2]])
+    >>> tol = 1e-5
+    >>> perplexity = 2.0
+    >>> P, beta = adjustbeta(X, tol, perplexity)
+    >>> print(P)
+    [[0.         0.50146484 0.49853516]
+    [0.50146484 0.         0.49853516]
+    [0.5        0.5        0.        ]]
+    >>> print(beta)
+    [[0.00195312]
+    [0.00195312]
+    [1.        ]]
     """
+    # Check if X is a 2D arrays
+    if X.ndim != 2:
+        raise ValueError("X must be a 2D array")
     (n, d) = X.shape
     # Need to compute D here, which is nxn distance matrix of X
+    from tsne import compute_pairwise_dist
+    D = compute_pairwise_dist(torch.from_numpy(X).float()).numpy()
     P = np.zeros((n, n))
     beta = np.ones((n, 1))
     logU = np.log(perplexity)
@@ -89,3 +125,19 @@ def adjustbeta(X, tol, perplexity):
         P[i, np.concatenate((np.r_[0:i], np.r_[i + 1 : n]))] = thisP
 
     return P, beta
+
+if __name__ == "__main__":
+    # Example usage
+    # Hbeta
+    D = np.array([[0, 1], [1, 0], [2, 2]])
+    beta = 1.0
+    H, P = Hbeta(D, beta)
+    print("H:", H)
+    print("P:", P)
+    # adjustbeta
+    X = np.array([[0, 1], [1, 0], [2, 2]])
+    tol = 1e-5
+    perplexity = 2.0
+    P, beta = adjustbeta(X, tol, perplexity)
+    print("P:", P)
+    print("beta:", beta)
